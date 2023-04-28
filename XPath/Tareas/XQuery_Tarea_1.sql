b5584e334ce507a6f4eb1777ac15db373336fd5d
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-04-28 19:43:25 +0100

    a

--- XPath/Tareas/XQuery_Tarea_1.sql
+++ XPath/Tareas/XQuery_Tarea_1.sql
@@ -17,7 +17,31 @@
 
 
 8. Mostrar profesores que den clases de bailes por una cuota mensual.
-
+for $baile in bailes/baile where $baile/precio/@cuota
 
 9. Mostrar el nombre de los bailes de la sala 1, que se pague con euros y el precio sea menor a 35.
+for $baile in bailes/baile where $baile/sala = 1 and $baile/precio/@moneda = 'euro' and $baile/precio > 35 
+return $baile/nombre/text
 
+10. Obtener el precio del baile con el precio más caro.
+
+
+11. Obtener el precio y el nombre del baile con el precio más caro.
+
+
+12. Obtener el precio del baile con el precio más barato.
+
+
+13. Obtener el precio y el nombre del baile con el precio más barato.
+
+
+14. Obtener la suma del precio por el número de plazas de todas las clases. Resultado: 29250.
+
+
+15. Obtener el dia, mes y año de los bailes mensuales, tanto del comienzo como del final.
+
+
+16. Obtener los bailes que tengan mas de 100 dias de diferencia.
+
+
+17. Obtener la diferencia de dias del comienzo del baile con la fecha actual.
